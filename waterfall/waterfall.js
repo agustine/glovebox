5f1bd38b5a98694e1d3f4204f28c180494dc6735
Ronnie <agustine103@gmail.com>
2014-08-03 14:18:53 +0800

    add waterfall

---
+++ waterfall/waterfall.js
@@ -0,0 +1,185 @@
+var Waterfall = (function(){
+	var options = {
+		margin: 0,
+		offsetH: 0,
+		offsetV: 0,
+		whiteBottom: 50,
+
+		cols: 0,
+		minWidth: 100,
+
+		ajaxUrl: '',
+		ajaxDataType: 'json',
+		ajaxData: {},
+
+		startPageIndex: 0,
+		pageIndexName: 'pno',
+		waterBox: 'li',
+		parse: function(res){
+			return (res instanceof Array) ? res : [];
+		},
+		url: function(item){
+			return item ? item.url : '';
+		},
+		template: function(imgData){
+			return '';
+		},
+		pageLoaded: function(res){
+		},
+		itemRendered: function(item){
+		},
+		last: function(res){
+			return false;
+		}
+	};
+	var pageIndex = 0, waterWidth, columns, waters, imgWidth, wrapper, minColumn = 0, noMore = false, fixed, wrapperWidth;
+	var log = (console && console.log) ? console.log : function(){};
+
+	function calculateColumnsHeight() {
+		var minH = Math.min.apply({}, columns);
+		var maxH = Math.max.apply({}, columns);
+		var columnCount = columns.length;
+		var wrapperHeight = (maxH + whiteBottom);
+		for(var i = 0; i < columnCount; i++){
+			if(columns[i] === minH){
+				minColumn = i;
+				break;
+			} 
+		}
+		wrapper.css('height', wrapperHeight.toString() + 'px');
+	};
+
+	function renderWater(item, imageWidth, imageHeight){
+		var itemElem = document.createElement(options.waterBox);
+		var itemHeight = (imgWidth / imageWidth) * imageHeight + options.offsetV;
+		var itemTop = columns[minColumn] + options.margin;
+		var itemLeft = (waterWidth + options.margin) * minColumn + options.margin;
+		itemElem.style.position = 'absolute';
+		itemElem.style.width = waterWidth + 'px';
+		itemElem.style.height = itemHeight + 'px';
+		itemElem.style.top = itemTop + 'px';
+		itemElem.style.left = itemLeft + 'px';
+		itemElem.innerHTML = options.template(item);
+
+		this.wrapper.appendChild(itemElem);
+		columns[minColumn] += itemHeight + options.margin;
+
+		calculateColumnsHeight();
+	};
+
+	function resetPosition(itemElem, imageWidth, imageHeight){
+		var itemHeight = (imgWidth / imageWidth) * imageHeight + options.offsetV;
+		var itemTop = columns[minColumn] + options.margin;
+		var itemLeft = (waterWidth + options.margin) * minColumn + options.margin;	
+		itemElem.style.width = waterWidth + 'px';
+		itemElem.style.height = itemHeight + 'px';
+		itemElem.style.top = itemTop + 'px';
+		itemElem.style.left = itemLeft + 'px';	
+
+		columns[minColumn] += itemHeight + options.margin;
+		calculateColumnsHeight();
+	};
+
+	function pushWater(item, imageWidth, imageHeight){
+		waters.push({
+			item: item,
+			width: imageWidth,
+			height: imageHeight
+		});
+	};
+
+	function callbackAjax(res){
+		var thisOptions = options;
+		var thisRenderWater = renderWater;
+		var thisPushWater = pushWater;
+		var pageData = options.parse(res);
+		var pageCount = pageData.length;
+		var item, url, img, doneFunc;
+		var done = 0;
+		noMore = options.last(res);
+		if(!res instanceof Array){
+			log('data parse error!');
+			return;
+		}
+
+		doneFunc = function(item){
+			done++;
+			if(done === pageCount){
+				thisOptions.pageLoaded(res);
+			}
+		}
+
+		for(var i = 0; i < pageCount; i++){
+			item = pageData[i];
+			url = options.url(item);
+			if(!(item && url)){
+				log('data parse error:');
+				log(item);
+				return;
+			}
+			img = new Image();
+
+			img.src = url;
+			img.onload=function(){
+			  thisRenderWater(item, this.width, this.height);
+			  thisPushWater(item, this.width, this.height);
+			  doneFunc();
+			};
+			img.onerror=function(){
+			  doneFunc();
+			};
+		};
+
+		pageLoaded(res);
+	};
+
+	function resetColumns(columnCount){
+		columns = [];
+		for(var i = 0; i < columnCount; i++){
+			columns.push(0);
+		}
+	};
+
+	function reset(cols){
+		var waterCount, itemDoms, elem, item;
+		fixed = cols ? true : fixed;
+		cols = cols || options.cols;
+		if(cols === options.cols && wrapperWidth === wrapper.clientWidth){
+			return;
+		}
+
+		wrapperWidth = wrapper.clientWidth;
+		if(!fixed){
+			cols = (wrapperWidth - options.margin) / options.minWidth
+		}
+		waterWidth = (wrapperWidth - ((options.cols + 1) * options.margin)) / cols;
+		imgWidth = waterWidth - options.offsetH;
+		resetColumns(cols);
+		options.cols = cols;
+
+		waterCount = waters.length;
+		itemDoms = wrapper.getElementsByTagName(options.waterBox);
+		for(var i = 0; i < waterCount; i++){
+			elem = itemDoms[i];
+			item = waters[i];
+			resetPosition(elem, item.width, item.height);
+		}
+	};
+
+	function load(){
+
+	};
+
+	function Waterfall(elemId, settings){
+		var i;
+		for (i in settings) options[i] = settings[i];
+	};
+
+	Waterfall.prototype.loadMore = load;
+
+	Waterfall.prototype.reset = function(cols){
+		reset(cols);
+	};
+
+	return Waterfall;
+})();
