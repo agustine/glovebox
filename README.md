34c143d261601f6fd1a68a448d9455fa6843d99e
Ronnie <agustine103@gmail.com>
2014-08-18 16:18:58 +0800

    add slideListener readme

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 代码杂物箱，一些之前写的前端jquery插件，组件...
 
 
+## slideListener
+
+为触屏设备浏览器添加划屏监听的jquery插件
+
+    $(selector)
+      .supportSlide(options) // 初始化
+      .on('slideUp', callbackSlideUp) // 添加上划监听
+      .on('slideDown', callbackSlideDown) // 添加下划监听
+      .on('slideLeft', callbackSlideLeft) // 添加左划监听
+      .on('slideRight', callbackSlideRight); // 添加右划监听
+    
+    options : {
+        pixel: 30, // 划越像素距离最小判定，默认30像素
+        interval: 0 // 2次划屏之间的有效判定毫秒数，防止用户一直划屏，造成的连续反复操作
+    }
--- slideListener/slideListener.js
+++ slideListener/slideListener.js
@@ -17,149 +17,149 @@
 
 (function ($) {
     'use strict';
-    // 2次有效划屏判定锁
-    var lock = false;
-    var options;
-    // 默认设定
-    var defaultOptions = {
-        pixel: 30, // 划越像素距离最小判定，默认30像素
-        interval: 0 // 2次划屏之间的有效判定毫秒数，防止用户一直划屏，造成的连续反复操作
-    };
-
-    /**
-     * 上锁
-     */
-    function lockSlide(){
-        lock = true;
-    };
-
-    /**
-     * 解锁
-     */
-    function unlockSlide(){
-        lock = false;
-    };
-
-    /**
-     * 出发划屏监听
-     * @param {jQuery Object}   $elem       监听elem的jQuery对象
-     * @param {String}          eventName   监听事件
-     */
-    function trigger($elem, eventName){
-        var interval = options.interval;
-        if(lock){
-            return;
-        }
-        $elem.trigger(eventName);
-        if(interval){
-            lockSlide();
-            setTimeout(unlockSlide, interval);
-        }
-    };
-
-    /**
-     * 判断方向，返回滑动事件名称
-     * @param  {Number} startX 起始点x轴像素值
-     * @param  {Number} startY 起始点y轴像素值
-     * @param  {Number} endX   结束点x轴像素值
-     * @param  {Number} endY   结束点y轴像素值
-     * @param  {Number} pixel  划越像素距离最小判定像素距离值
-     * @return {String}        返回滑动事件名称
-     */
-    function direction (startX, startY, endX, endY, pixel) {
-        'use strict';
-        var pixelX = endX - startX;
-        var pixelY = endY - startY;
-        var absX = Math.abs(pixelX);
-        var absY = Math.abs(pixelY);
-        pixel = Math.abs(pixel);
-        // x轴 y轴都小于判定值，放回空淄川
-        if (Math.max(absX, absY) < pixel) {
-            return '';
-        }
-        if (absY > absX) {
-            if (pixelY > 0) {
-                return 'slideDown';
-            } else {
-                return 'slideUp';
-            }
-        } else {
-            if (pixelX > 0) {
-                return 'slideRight';
-            } else {
-                return 'slideLeft';
-            }
-        }
-    }
-
-//    function touchStart(e){
-//
-//    }
-//
-//    function touchMove(e){
-//
-//    }
-//
-//    function touchEnd(e){
-//
-//    }
+    var isIE = (/iemobile/gi).test(navigator.appVersion);
 
     $.fn.extend({
         supportSlide: function (settings) {
             'use strict';
+            // touchstart时触电坐标x、y
+            var startX,
+                startY,
+            // 2次有效划屏判定锁
+                lock = false,
+            // 触摸事件监听名称
+                startEvent = isIE ? 'MSPointerDown' : 'touchstart',
+                moveEvent = isIE ? 'MSPointerMove' : 'touchmove',
+                endEvent = isIE ? 'MSPointerUp' : 'touchend',
+            // 默认设定
+                options = {
+                    pixel: 30, // 划越像素距离最小判定，默认30像素
+                    interval: 0 // 2次划屏之间的有效判定毫秒数，防止用户一直划屏，造成的连续反复操作
+                },
+                trigger,
+                that = $(this).get(0);
             // 获取options
-            options = $.extend(defaultOptions, settings);
+            options = $.extend(options, settings);
 
-            // 非ie浏览器标准监听
-            $(this).on('touchstart', function (e) {
-                'use strict';
-                var $this = $(this);
-                var pixel = options.pixel;
-                var touch, startX, startY, endX, endY, eventName;
-                e.originalEvent = e.originalEvent || e;
-                touch = e.originalEvent.changedTouches[0];
-                startX = touch.pageX;
-                startY = touch.pageY;
+            /**
+             * 上锁
+             */
+            function lockSlide() {
+                lock = true;
+            };
 
-                $this.bind('touchmove', function (e) {
-                    'use strict';
-                    e.originalEvent = e.originalEvent || e;
-                    touch = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
-                    endX = touch.pageX;
-                    endY = touch.pageY;
-                    eventName = direction(startX, startY, endX, endY, pixel);
-                    eventName && $this.off('touchmove');
-//                    eventName && $this.trigger(eventName);
-                    eventName && trigger($this, eventName);
-                });
-                return true;
-            }).on('touchend', function (e) {
-                'use strict';
-                $(e.currentTarget).off('touchmove');
-                return true;
-            });
+            /**
+             * 解锁
+             */
+            function unlockSlide() {
+                lock = false;
+            };
 
-            // ie mobile
-            $(this).get(0).addEventListener('MSPointerDown', function (event) {
+            /**
+             * 出发划屏监听
+             * @param {jQuery Object}   $elem       监听elem的jQuery对象
+             * @param {String}          eventName   监听事件
+             */
+            function triggerFunc($elem, eventName) {
+                var interval = options.interval;
+                if (lock) {
+                    return;
+                }
+                $elem.trigger(eventName);
+                if (interval) {
+                    lockSlide();
+                    setTimeout(unlockSlide, interval);
+                }
+            };
+
+            /**
+             * 判断方向，返回滑动事件名称
+             * @param  {Number} startX 起始点x轴像素值
+             * @param  {Number} startY 起始点y轴像素值
+             * @param  {Number} endX   结束点x轴像素值
+             * @param  {Number} endY   结束点y轴像素值
+             * @param  {Number} pixel  划越像素距离最小判定像素距离值
+             * @return {String}        返回滑动事件名称
+             */
+            function direction(startX, startY, endX, endY) {
                 'use strict';
-                var $this = $(this);
-                var pixel = options.pixel;
-                var startX, startY, endX, endY, eventName;
-                startX = event.pageX;
-                startY = event.pageY;
-                this.addEventListener('MSPointerMove', function (event) {
+                var pixelX = endX - startX;
+                var pixelY = endY - startY;
+                var absX = Math.abs(pixelX);
+                var absY = Math.abs(pixelY);
+                var pixel = Math.abs(options.pixel);
+                // x轴 y轴都小于判定值，放回空淄川
+                if (Math.max(absX, absY) < pixel) {
+                    return '';
+                }
+                if (absY > absX) {
+                    if (pixelY > 0) {
+                        return 'slideDown';
+                    } else {
+                        return 'slideUp';
+                    }
+                } else {
+                    if (pixelX > 0) {
+                        return 'slideRight';
+                    } else {
+                        return 'slideLeft';
+                    }
+                }
+            }
+
+            /**
+             * touch start 事件
+             * @param {Object} event 事件对象
+             */
+            function touchStart(event) {
+                var touch;
+                if (isIE) {
+                    startX = event.pageX;
+                    startY = event.pageY;
+                } else {
+                    event.originalEvent = event.originalEvent || event;
+                    touch = event.originalEvent.touches[0];
+                    startX = touch.pageX;
+                    startY = touch.pageY;
+                }
+                trigger = triggerFunc;
+            }
+
+            /**
+             * touch move 事件
+             * @param {Object} event 事件对象
+             */
+            function touchMove(event) {
+                var touch, endX, endY, eventName;
+                // 获取当前触摸位置
+                if (isIE) {
                     endX = event.pageX;
                     endY = event.pageY;
-                    eventName = direction(startX, startY, endX, endY, pixel);
-                    eventName && $this.off('MSPointerMove');
-//                    eventName && $this.trigger(eventName);
-                    eventName && trigger($this, eventName);
-                }, false);
-            }, false);
-            $(this).get(0).addEventListener('MSPointerUp', function (event) {
-                'use strict';
-                $(this).off('MSPointerMove');
-            }, false);
+                } else {
+                    event.originalEvent = event.originalEvent || event;
+                    touch = event.originalEvent.touches[0];
+                    endX = touch.pageX;
+                    endY = touch.pageY;
+                }
+                event.preventDefault();
+                // 获取划屏方向
+                eventName = direction(startX, startY, endX, endY);
+                // 触发监听
+                eventName && trigger && trigger($(this), eventName);
+            }
+
+            /**
+             * touch end 事件
+             * @param {Object} event 事件对象
+             */
+            function touchEnd(event) {
+                trigger = null;
+            }
+
+            // 添加 touchstart touchmove touchend监听
+            that.addEventListener(startEvent, touchStart, false);
+            that.addEventListener(moveEvent, touchMove, false);
+            that.addEventListener(endEvent, touchEnd, false);
 
             return this;
         }
